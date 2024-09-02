8c2e15d7eff7564c55157685e9c1a927cffc2a71
vijayraj627 <vijayraj627@gmail.com>
2024-09-02 23:50:09 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { api } from './utils.js/constant';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch(api);
+      const json = await res.json();
+      setData(json)
+      console.log(json)
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item,index)=>{
+        return <p key={index}>{item.email}</p>
+      })}
     </div>
   );
 }
